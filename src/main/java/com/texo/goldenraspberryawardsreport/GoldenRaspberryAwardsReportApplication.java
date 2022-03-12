d215aecb05091e48ba19c2888e0d0a3f777f056e
Alessandro Ceron <alessandro.ceron@cwi.com.br>
2022-03-12 11:36:00 -0300

    Criação do projeto

---
+++ src/main/java/com/texo/goldenraspberryawardsreport/GoldenRaspberryAwardsReportApplication.java
@@ -0,0 +1,13 @@
+package com.texo.goldenraspberryawardsreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryAwardsReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenRaspberryAwardsReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/texo/goldenraspberryawardsreport/GoldenRaspberryAwardsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texo.goldenraspberryawardsreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
